e33cff09bf93d984eba82348501872d6e01d1354
yeshuakas <yeshuakas@gmail.com>
2026-08-10 11:32:56 -0600

    Update read.js

--- cmds/utils/read.js
+++ cmds/utils/read.js
@@ -1,4 +1,4 @@
-import { downloadContentFromMessage, extractMessageContent } from 'baileys';
+import { downloadContentFromMessage, extractMessageContent } from '@whiskeysockets/baileys';
 
 export default {
   command: ['readviewonce', 'read', 'readvo'],
